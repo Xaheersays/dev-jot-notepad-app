abce2664cfe0ddd98d9fa708a3df33009b7eb53d
Zaheer <105771794+Xaheersays@users.noreply.github.com>
2023-05-31 16:06:07 +0530

    Delete app-react directory

--- app-react/src/components/TextIcon/TextIcon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TextIcon = ({ icon, text, description }) => (
-  <h2 style={{ color: 'white' }}>
-    <img alt={description} src={icon} /> {text}
-  </h2>
-);
-
-export default TextIcon;
